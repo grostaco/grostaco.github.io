8126ab0855ac3029be7cfabbc4789fda07d99741
grostaco <grostaco@gmail.com>
2022-01-17 16:44:19 +0700

    feat: add counter

--- app.js
+++ app.js
@@ -27,6 +27,30 @@ function hide(elem) {
     gsap.set(elem, { autoAlpha: 0 });
 }
 
+function counter(day, hour, minute, second, initial, start, end, duration) {
+    start = start || 0;
+    let startTimestamp = null;
+
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        let elapsed = Math.floor(progress * (end - initial));
+
+        day.innerHTML = Math.floor(elapsed / 86400);
+        hour.innerHTML = Math.floor(elapsed % 86400 / 3600);
+        minute.innerHTML = Math.floor(elapsed % 86400 % 3600 / 60);
+        second.innerHTML = elapsed % 60;
+
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        } else {
+            //setTimeout(() => counter(day, hour, minute, second, initial, end, Math.floor(Date.now() / 1000), duration), 100);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
     gsap.registerPlugin(ScrollTrigger);
 
@@ -41,3 +65,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const days = document.getElementById("days");
+    const hours = document.getElementById("hours");
+    const minutes = document.getElementById("minutes");
+    const seconds = document.getElementById("seconds");
+    counter(days, hours, minutes, seconds, 1621459200, 1621459200, Math.floor(Date.now() / 1000), 5000);
+})
